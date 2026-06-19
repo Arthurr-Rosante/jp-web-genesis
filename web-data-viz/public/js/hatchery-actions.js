82c258f754df048b5e2994d5a178478d2fcbac21
arthurr-rosante <arrosante@gmail.com>
2026-06-19 14:08:12 -0300

    feat: incubate and hatchTimer actions

---
+++ web-data-viz/public/js/hatchery-actions.js
@@ -0,0 +1,88 @@
+function incubateSpecies(species) {
+    let gameData = storage.get("JPWG_DATA");
+    if (!gameData || !gameData.park) {
+        toast({ 
+            variant: "destructive", 
+            title: "Erro", 
+            message: "Dados estão corrompidos. Tente logar-se novamente."
+        });
+        return;
+    }
+
+    const {park} = gameData;
+
+    // Procura primeiro slot vazio
+    const emptySlot = document.querySelector(".slot--empty");
+    if(!emptySlot) {
+        toast({ 
+            variant: "warn", 
+            title: "Operação Negada", 
+            message: `Incubadora cheia! Não há espaços para incubar ${species.name}!` 
+        });
+        return;
+    }
+
+    // Checa insuficiência do saldo
+    const newBalance = park.balance - species.hatchCost;
+    if (newBalance < 0) {
+        toast({
+            variant: "warn",
+            title: "Operação Negada",
+            message: "Você não possui saldo suficiente!"
+        });
+        return;
+    }
+    
+    // Atualiza sessionStorage
+    park.balance = newBalance;
+    storage.set("JPWG_DATA", gameData);
+
+    // Cria timer de incubação
+    const timeToHatch = (BASE_HATCH_INTERVAL_MS * (species.ratingWeight))
+    let timeLeft = timeToHatch;
+
+    const hatchTimer = createTimer(`hatchery-${emptySlot.id}`, () => {
+        let progress =  Math.round(((timeToHatch - timeLeft) / timeToHatch) * 100);
+
+        loadHatcherySlots();
+        let slotContent = emptySlot.querySelector(".slot-content");
+        if(slotContent) {
+            const spanHatchProgress = document.createElement("span");
+            spanHatchProgress.className = "slot-hatch-progress";
+            spanHatchProgress.innerHTML = `${progress}%`;
+
+            slotContent.appendChild(spanHatchProgress);
+        }
+        
+        if(timeLeft <= 0) {
+            hatchSpecies(species, emptySlot);
+            return;
+        }
+        
+        timeLeft -= 1000;
+    }, 1000);
+
+    // Altera status do slot de incubação
+    emptySlot.classList.replace("slot--empty", "slot--hatching");
+    emptySlot.classList.add(`hatching--${species.name}`);
+
+    // Atualiza UI
+    loadStatusbar();
+    loadHatcherySlots();
+
+    toast({
+        variant: "success",
+        title: "Incubando",
+        message: `${species.name} está sendo incubado!`
+    });
+}
+
+function hatchSpecies(species, slot) {
+    const timer = activeTimers.get(`hatchery-${slot.id}`);
+    if(timer) timer.destroy();
+    
+    slot.classList.replace("slot--hatching", "slot--done");
+    console.log("INCUBADO!");
+    console.log(species);
+    loadHatcherySlots();
+}
--- web-data-viz/public/js/loaders/hatchery.js
+++ web-data-viz/public/js/loaders/hatchery.js
@@ -1,1 +1,134 @@
-function loadHatchery() {}
+async function loadHatchery() {
+    const hatcheryList = document.getElementById("hatchery-list");
+    if(!hatcheryList) return;
+    hatcheryList.innerHTML = "";
+
+    // Busca dados do sessionStorage
+    const gameData = storage.get("JPWG_DATA");
+    if(!gameData) {
+        toast({
+            variant: "destructive",
+            title: "Erro",
+            message: "Dados estão corrompidos. Tente logar-se novamente."
+        });
+        return;
+    }
+
+    // Busca espécies de dinossauro
+    const species = await fetch(`/api/species`)
+    .then((res) => res.json().then((data) => {
+        if(!res.ok) throw data.error;
+        return data.species;
+    }))
+    .catch((error) => {
+        toast({
+            variant: "destructive",
+            title: "Erro ao retornar Espécies",
+            message: error
+        });
+    });
+
+    // Carrega lista de espécies para incubação
+    species.forEach(sp => {
+       const speciesCard = document.createElement("li");
+       speciesCard.className = "hatchery-item";
+       speciesCard.style.backgroundImage = `url(${speciesDataMap[sp.name].iconUrl})`;
+        speciesCard.innerHTML = speciesCardHTML(sp);
+        
+        const incubateBtn = speciesCard.querySelector(".hatch-btn");
+        if(!incubateBtn) return;
+        incubateBtn.onclick = () => incubateSpecies(sp);
+
+        hatcheryList.appendChild(speciesCard);
+    });
+
+};
+
+function speciesCardHTML(species) {
+    return `
+        <p class="hatchery-title">${species.name.charAt(0).toUpperCase() + species.name.slice(1)}</p>
+        <div class="hatchery-item-info">
+            <p><span><i class="ph-fill ph-coins"></i> ${species.hatchCost}</span></p>
+            <p><span><i class="ph-fill ph-egg-crack"></i> ${formatDinoHatchSuccess(species.hatchSuccessRate)}</span></p>
+        </div>
+        <div class="hatchery-item-footer">
+            <button class="hatch-btn button--primary">Incubar</button>
+        </div>
+    `;
+}
+
+function loadHatcherySlots() {
+    const hatcherySlots = document.querySelectorAll(".hatchery-slot");
+    hatcherySlots.forEach((slot) => {
+        // slot.classList[1], pois: ['hatchery-slot', 'slot--hatching', 'hatching--tiranossauro']; 
+        const status = slot.classList[1].split("--")[1];
+        const speciesName = slot.classList[2]?.split("--")[1];
+
+        if(status === "empty" || !speciesName) {
+            slot.innerHTML = "";
+            return;
+        };
+        
+        const speciesData = speciesDataMap[speciesName];
+        slot.innerHTML = hatcherySlotHTML(status, speciesData);
+
+        if(status === "done") {
+            const placeBtn = slot.querySelector(".place-dino-btn"); 
+            placeBtn.onclick = () => {
+                console.log("COLOCANDO DINO!");
+                console.log(speciesData);
+
+                // Atualiza UI
+                slot.className = "hatchery-slot slot--empty";
+                loadHatcherySlots();
+            };
+            
+            const discardBtn = slot.querySelector(".discard-dino-btn"); 
+            discardBtn.onclick = () => {
+                console.log("DESCARTANDO DINO!");
+                console.log(speciesData);
+
+                // Atualiza UI
+                slot.className = "hatchery-slot slot--empty";
+                loadHatcherySlots();
+            };
+        }
+    });
+}
+
+function hatcherySlotHTML(status, speciesData) {
+    if(status === "hatching") {
+        return `
+            <div class="slot-dinosaur" style="background-image: url(${speciesData.iconUrl});">
+                <div class="slot-content">
+                    <p>Progresso da Incubação</p>
+                </div>
+            </div>
+        `;
+    };
+
+    if(status === "done") {
+        const gameData = storage.get("JPWG_DATA");
+        let availableEnclosuresList = 
+            gameData.tiles.filter((t) => t.category === "enclosure" && t.idSpecies === null);
+
+        const enclosureOptionHTML = (tile) => `
+            <option value="r${tile.positionRow}_c${tile.positionCol}">
+            ${buildingsDataMap[tile.name].translatedName} (L: ${tile.positionRow + 1} | C: ${tile.positionCol + 1})
+            </option>
+        `;
+
+        return `
+            <div class="slot-dinosaur" style="background-image: url(${speciesData.iconUrl});">
+                <button class="discard-dino-btn"><p class="ph-fill ph-trash"></p></button>
+                <div class="slot-content">
+                    <select class="ipt-place-dinosaur">
+                    ${availableEnclosuresList.map((tile) => enclosureOptionHTML(tile)).join("")}
+                    </select>
+                    <button class="place-dino-btn">Colocar Dinossauro</button>
+                </div>
+            </div>
+        `;
+    };
+
+}

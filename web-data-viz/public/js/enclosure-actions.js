f05f44917106c95ceb16f59f22db2be5052beffa
arthurr-rosante <arrosante@gmail.com>
2026-06-20 18:34:33 -0300

    feat: successfully adapt enclosure logic from old repo (with a few tweaks)

---
+++ web-data-viz/public/js/enclosure-actions.js
@@ -0,0 +1,258 @@
+function goToHatchery() {
+    const gameData = storage.get("JPWG_DATA");
+    if(!gameData || !gameData.tiles) {
+        toast({ 
+            variant: "destructive", 
+            title: "Erro", 
+            message: "Dados estão corrompidos. Tente logar-se novamente."
+        });
+        return
+    }
+
+    const hasHatchery = gameData.tiles.some((t) => t.name === "hatchery");
+    if(hasHatchery) {
+        togglePanel("enclosure-panel");
+        togglePanel("hatchery-panel", {onToggle: loadHatchery});
+    } else {
+        toast({ 
+            variant: "warn", 
+            title: "Operação Negada", 
+            message: "Seu parque não possui nenhum laboratório de incubação."
+        });
+    }
+
+}
+
+function repairEnclosure(tile) {
+    if(tile.currentHp >= tile.maxHp) {
+        toast({
+            variant: "warn",
+            title: "Operação Negada",
+            message: `${buildingsDataMap[tile.name].translatedName} já está um brinco!`
+        });
+        return;
+    }
+
+    let gameData = storage.get("JPWG_DATA");
+    if(!gameData || !gameData.park) {
+        toast({
+            variant: "destructive",
+            title: "Erro",
+            message: "Dados estão corrompidos. Tente logar-se novamente."
+        });
+        return;
+    }
+
+    const {park} = gameData;
+
+    // Checa insuficiência do saldo
+    const newBalance = park.balance - (tile.baseCost * REPAIR_FEE); 
+    if(newBalance < 0) {
+        toast({
+            variant: "warn",
+            title: "Operação Negada",
+            message: "Você não possui saldo suficiente!"
+        });
+        return;
+    }
+    
+    // Calcula novo HP do cercado (mantém na faixa 0-100)
+    const newHp = Math.max(0, Math.min(100, tile.currentHp + BASE_REPAIR_POINTS));
+    
+    fetch(`/api/tiles/${tile.idPark}`, {
+        method: "PUT",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({
+            tiles: [
+                {
+                    positionRow: tile.positionRow,
+                    positionCol: tile.positionCol,
+                    currentHp: newHp
+                }
+            ]
+        })
+    })
+    .then(res => res.json().then(data => {
+        if(!res.ok) {
+            throw data.error;
+        }
+
+        // atualiza sessionStorage
+        const updatedTiles = data.tiles;
+
+        gameData.park.balance = newBalance;
+        gameData.tiles = updatedTiles;
+        storage.set("JPWG_DATA", gameData);
+
+        // Atualiza UI
+        loadStatusbar();
+        loadEnclosure(data.updated[0]);
+
+        console.log(`Evento: ${tile.name} reparado às ${new Date().toLocaleTimeString()}`);
+    }))
+    .catch((error) => {
+        toast({
+            variant: "destructive",
+            title: "Erro ao reparar cercado",
+            message: error
+        })
+    })
+}
+
+function upgradeEnclosure(tile) {
+    // Checa se Tile possui upgrades
+    if(!tile.idUpgrade) {
+        toast({
+            variant: "warn",
+            title: "Operação Negada",
+            message: `${buildingsDataMap[tile.name].translatedName} não possui mais melhorias!`
+        });
+        return;
+    }
+
+    // Checa se Tile foi devidamente consertado
+    if(tile.currentHp < tile.maxHp) {
+        toast({
+            variant: "warn",
+            title: "Operação Negada",
+            message: `${buildingsDataMap[tile.name].translatedName} precisa estar 100% consertado!`
+        });
+        return;
+    }
+
+    let gameData = storage.get("JPWG_DATA");
+    if(!gameData || !gameData.park) {
+        toast({
+            variant: "destructive",
+            title: "Erro",
+            message: "Dados estão corrompidos. Tente logar-se novamente."
+        });
+        return;
+    }
+
+    const {park} = gameData;
+
+    // Checa insuficiência do saldo
+    const newBalance = park.balance - tile.upgradeCost; 
+    if(newBalance < 0) {
+        toast({
+            variant: "warn",
+            title: "Operação Negada",
+            message: "Você não possui saldo suficiente!"
+        });
+        return;
+    }
+    
+    fetch(`/api/tiles/${tile.idPark}`, {
+        method: "PUT",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({
+            tiles: [
+                {
+                    positionRow: tile.positionRow,
+                    positionCol: tile.positionCol,
+                    idBuilding: tile.idUpgrade
+                }
+            ]
+        })
+    })
+    .then(res => res.json().then(data => {
+        if(!res.ok) {
+            throw data.error;
+        }
+
+        // atualiza sessionStorage
+        const updatedTiles = data.tiles;
+
+        gameData.park.balance = newBalance;
+        gameData.tiles = updatedTiles;
+        storage.set("JPWG_DATA", gameData);
+
+        // Atualiza UI
+        loadGameUI();
+        loadEnclosure(data.updated[0]);
+
+        toast({
+            variant: "success",
+            title: "Construção Melhorada",
+            message: `${buildingsDataMap[tile.name].translatedName} foi melhorado para ${buildingsDataMap[data.updated[0].name].translatedName}!`
+        });
+        console.log(`Evento: ${tile.name} melhorado para ${data.updated[0].name} às ${new Date().toLocaleTimeString()}`);
+    }))
+    .catch((error) => {
+        toast({
+            variant: "destructive",
+            title: "Erro ao melhorar cercado",
+            message: error
+        })
+    })
+}
+
+function releaseDinosaur(tile) {
+    // Checa se Tile possui algum espécime
+    if(!tile.idSpecies || !tile.dinosaur) return
+
+    // Checa se Tile foi devidamente consertado
+    if(tile.currentHp < tile.maxHp) {
+        toast({
+            variant: "warn",
+            title: "Operação Negada",
+            message: `${buildingsDataMap[tile.name].translatedName} precisa estar 100% consertado!`
+        });
+        return;
+    }
+
+    let gameData = storage.get("JPWG_DATA");
+    if(!gameData || !gameData.park) {
+        toast({
+            variant: "destructive",
+            title: "Erro",
+            message: "Dados estão corrompidos. Tente logar-se novamente."
+        });
+        return;
+    }
+
+    fetch(`/api/tiles/${tile.idPark}`, {
+        method: "PUT",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({
+            tiles: [
+                {
+                    positionRow: tile.positionRow,
+                    positionCol: tile.positionCol,
+                    idSpecies: null
+                }
+            ]
+        })
+    })
+    .then(res => res.json().then(data => {
+        if(!res.ok) {
+            throw data.error;
+        }
+
+        // atualiza sessionStorage
+        const updatedTiles = data.tiles;
+
+        gameData.tiles = updatedTiles;
+        gameData.park.rating = gameData.park.rating - tile.dinosaur.ratingWeight;
+        storage.set("JPWG_DATA", gameData);
+
+        // Atualiza UI
+        loadGameUI();
+        loadEnclosure(data.updated[0]);
+
+        toast({
+            variant: "success",
+            title: "Espécime Removido",
+            message: `${tile.dinosaur.name} foi solto com sucesso!`
+        });
+        console.log(`Evento: ${tile.dinosaur.name} solto de ${tile.name} às ${new Date().toLocaleTimeString()}`);
+    }))
+    .catch((error) => {
+        toast({
+            variant: "destructive",
+            title: "Erro ao soltar espécime",
+            message: error
+        })
+    })
+}
--- web-data-viz/public/js/loaders/enclosure.js
+++ web-data-viz/public/js/loaders/enclosure.js
@@ -1,1 +1,122 @@
-function loadEnclosure(tile) {}
+function loadEnclosure(tile) {
+    if(tile.category !== "enclosure") return;
+
+    const panel = document.getElementById("enclosure-panel");
+    if(!panel) return;
+    panel.innerHTML = "";
+
+    if(tile.idSpecies) {
+        // Carrega UI de cercado com dinossauro
+        panel.innerHTML = occupiedEnclosureHTML(tile);
+    } else {
+        // Carrega UI de cercado vazio
+        panel.innerHTML = emptyEnclosureHTML(tile);
+    }
+
+    // Eventos HTML
+    const goToHatcheryBtn = document.getElementById("go-to-hatchery-btn");
+    if(goToHatcheryBtn) {
+        goToHatcheryBtn.onclick = goToHatchery;
+    }
+    
+    const repairEnclosureBtn = document.getElementById("repair-enclosure-btn");
+    if(repairEnclosureBtn) {
+        repairEnclosureBtn.onclick = () => repairEnclosure(tile);
+    }
+    
+    const upgradeEnclosureBtn = document.getElementById("upgrade-enclosure-btn");
+    if(upgradeEnclosureBtn) {
+        upgradeEnclosureBtn.onclick = () => upgradeEnclosure(tile);
+    }
+    
+    const releaseDinoBtn = document.getElementById("release-dinosaur-btn");
+    if(releaseDinoBtn) {
+        releaseDinoBtn.onclick = () => releaseDinosaur(tile);
+    }
+
+
+}
+
+function occupiedEnclosureHTML(tile) {
+    const dino = tile.dinosaur;
+
+    return `
+        <h3 class="panel-title">Cercado de ${dino.name}</h3>
+        <div class="enclosure-dinosaur">
+            <img src="${speciesDataMap[dino.name].spriteUrl}" alt="${dino.name}" class="species-img">
+            <div class="enclosure-dinosaur-card">
+                <div class="card-image">
+                    <img src="${speciesDataMap[dino.name].iconUrl}" alt="${dino.name}" class="species-icon">
+                    <img src="./assets/images/frame.png" alt="frame" class="frame">
+                </div>
+                <button id="release-dinosaur-btn" class="button--accent">Soltar ${dino.name}</button>
+            </div>
+        </div>
+        <div class="enclosure-status">
+            <div class="enclosure-status-header">
+                <h2>${buildingsDataMap[tile.name].translatedName}</h2>
+            </div>
+            <div class="enclosure-status-body">
+                <div class="enclosure-radar">
+                    <canvas id="enclosure-radar-chart"></canvas>
+                </div>
+                <div class="enclosure-info">
+                    <ul>
+                        <li>Era: <span>${dino.temporalRange}</span></li>
+                        <li>Locomoção: <span>${dino.locomotionType}</span></li>
+                        <li>Dieta: <span>${dino.diet}</span></li>
+                        <li>Altura: <span>${formatDinoHeight(Number(dino.heightInMeters))}</span></li>
+                        <li>Peso: <span>${formatDinoWeight(Number(dino.weightInKilograms))}</span></li>
+                        <li>Agressividade: <span>${formatDinoAggressiveness(dino.aggressiveness)}</span></li>
+                    </ul>
+                </div>
+            </div>
+            ${enclosureFooterHTML(tile)}
+        </div>
+    `;
+}
+
+function emptyEnclosureHTML(tile) {
+    return `
+        <h3 class="panel-title">Cercado Vazio</h3>
+        <div class="enclosure-dinosaur"></div>
+        <div class="enclosure-status">
+            <div class="enclosure-status-header">
+                <h2>${buildingsDataMap[tile.name].translatedName}</h2>
+            </div>
+            <div class="enclosure-status-body">
+                <div class="empty-enclosure">
+                    <p>Cercado Vazio</p>
+                    <button id="go-to-hatchery-btn">Incubar Espécime</button>  
+                </div>
+            </div>
+            ${enclosureFooterHTML(tile)}
+        </div>
+    `;
+}
+
+function enclosureFooterHTML(tile) {
+    return `
+        <div class="enclosure-status-footer">
+            ${enclosureHealthbarHTML(tile)}
+            <div class="enclosure-actions">
+                <button id="repair-enclosure-btn" class="button--secondary"><i class="ph-fill ph-wrench"></i>Reparar (${tile.baseCost * REPAIR_FEE})</button>
+                ${tile.idUpgrade ? `<button id="upgrade-enclosure-btn" class="button--primary"><i class="ph-fill ph-coins"></i> Melhorar (${tile.upgradeCost})</button>` : ""}
+            </div>
+        </div>
+    `;
+}
+
+function enclosureHealthbarHTML(tile) {
+    const hpInPercentage = Math.round((tile.currentHp / tile.maxHp) * 100);
+
+    return `
+        <div class="enclosure-health">
+            <span>HP:</span>
+            <div class="enclosure-healthbar-wrapper">
+                <div id="enclosure-healthbar" style="width:${hpInPercentage}%;"></div>
+            </div>
+            <span>${tile.currentHp}/${tile.maxHp}</span>
+        </div>
+    `;
+}
